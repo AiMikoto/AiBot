5f9caa08a6d1fa8d0b07daafc1bd6d66d4d490dd
Mihai-Lucian Voncilă <mihai.voncila@gmail.com>
2019-11-18 00:49:46 +0200

    Really stupid token

--- Runner.py
+++ Runner.py
@@ -4,7 +4,7 @@ import AiCommands as ai_cmd
 
 class Runner:
     def __init__(self):
-        self.token = 'NjQwNzAyODExMTU5NjU4NTA4.XdHL2g.p4ivpDDXYNxfqJ2jN6GqwKMOHlQ'
+        self.token = 'NjQwNzAyODExMTU5NjU4NTA4.XdHOJA.cn7S7ExGctweZg06MzLPnDf-rXY'
         self.client = commands.Bot(command_prefix = 'ai!')
         self.add_commands()
         self.on_bot_ready()
